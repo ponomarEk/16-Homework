4345a0c8df30f510da887076a75f4392dbd34e2d
Sasha <ponomarek91@gmail.com>
2020-03-31 00:44:06 +0300

    finish

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Contacts from './components/Contacts/contacts'
+import './App.css';
+
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Contacts  /> 
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/Contacts/search.js
@@ -0,0 +1,20 @@
+// import React , { Component } from 'react';
+// import './search.css';
+// class Search extends Component{
+//    state={
+//        val : '',
+//        contacts :this.props.att,
+//        data:'',
+//        flag:'passive'
+//    }
+  
+// }
+//     render(){
+        
+//         return(
+            
+//         )
+//     }
+// }
+
+// export default Search;
